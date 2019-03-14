dbf42587e949f07675565a0f6b8322b9aa4183bf
TessavWalstijn <twalstijn@hotmail.com>
2019-03-14 13:36:51 +0100

    added Source with scripts

---
+++ Assets/Source/VisualConnections.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualConnections : MonoBehaviour
+{
+    
+    [SerializeField]
+    private Color _open;
+
+    [SerializeField]
+    private Color _closed;
+
+    private Waypoints _waypoints;
+    private Material[] _matirials = new Material[10];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _waypoints = GetComponent<Waypoints>();
+        GameObject[] waypoints = _waypoints.waypoints;
+
+        int maxWaypoints = waypoints.Length;
+        for (int i = 0; i < maxWaypoints; i += 1) {
+            GameObject point = waypoints[i];
+            Renderer render = point.GetComponent<Renderer>();
+            _matirials[i] = render.GetComponent<Material>();
+        }
+    }
+
+    public void ShowAvailbleConnections (int waypoint)
+    {
+        int[] connections = _waypoints.GetAvailbleConnections(waypoint);
+
+        int maxWaypoints = _matirials.Length;
+        for (int i = 0; i < maxWaypoints; i += 1) {
+            _matirials[i].color = _closed;
+        }
+
+        int maxConnections = connections.Length;
+        for (int i = 0; i < maxConnections; i += 1) {
+            _matirials[i].color = _open;
+        }
+    }
+}
