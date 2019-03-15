495b94a573ec2da6009f28fd66223113897ae9cb
ttking <tschumacher1997@gmail.com>
2019-03-15 09:12:40 +0100

    Menumanager
    First upload of finished untested menu manager

---
+++ Assets/Scripts/Menumanager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menumanager : MonoBehaviour
+{
+    public List<GameObject> MenuList;
+    public Transform parent;
+
+    private GameObject MenuContainers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MenuContainers = new GameObject();
+        ActivateMenu(0);
+
+        foreach (GameObject menu in MenuList)
+        {
+            if(menu != null)
+            {
+                menu.transform.SetParent(MenuContainers.transform);
+            }
+        }
+    }
+
+    public void ActivateMenu(int menuID)
+    {
+        foreach( var menu in MenuList)
+        {
+            if(menu != null)
+            {
+                menu.SetActive(false);
+            }
+        }
+
+        if (MenuList[menuID] != null)
+        {
+            MenuList[menuID].SetActive(true);
+        }
+    }
+}
